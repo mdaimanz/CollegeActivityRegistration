fb4819e7fe8fdaf576cd7005cbc0d3b9dbd85aff
mdaimanz <39907470+mdaimanz@users.noreply.github.com>
2020-07-20 22:11:53 +0800

    Update home.php

--- php/home.php
+++ php/home.php
@@ -8,6 +8,7 @@ session_start();
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 
