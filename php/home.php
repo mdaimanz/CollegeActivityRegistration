de5f65b215162d427dcbede65aebc9c6e7140ebe
mdaimanz <39907470+mdaimanz@users.noreply.github.com>
2020-07-21 01:34:03 +0800

    Update home.php

--- php/home.php
+++ php/home.php
@@ -16,7 +16,7 @@ header("Access-Control-Allow-Origin: *");
   <link rel="stylesheet" type="text/css" href="../css/login.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link href="https://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+  <link  rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie" type="text/css">
   <!-- <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script> -->
 
 
